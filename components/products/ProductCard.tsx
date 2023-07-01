c44b9899c9ffa1127ba764c5f07368b7ac102ec0
Andrés Murillo Cortázar <andresmurillocortazar@outlook.com>
2023-07-01 13:25:46 -0500

    Class 187

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,21 +1,55 @@
-import { IProduct } from "@/interfaces"
-import { Grid, CardActionArea, CardMedia } from "@mui/material"
-import { FC } from "react"
+import { IProduct } from "@/interfaces";
+import NextLink from 'next/link'
+import {
+  Grid,
+  CardActionArea,
+  CardMedia,
+  Card,
+  Box,
+  Typography,
+  Link,
+} from "@mui/material";
+import { FC, useMemo, useState } from "react";
 
-interface Props{
-  product: IProduct,
+interface Props {
+  product: IProduct;
 }
 
-export const ProductCard:FC<Props> = ({product}) => {
+export const ProductCard: FC<Props> = ({ product }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const productImage = useMemo(() =>{
+    return isHovered 
+    ? `products/${product.images[1]}` 
+    : `products/${product.images[0]}`;
+  }, [isHovered, product.images])
+
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Card>
+        <NextLink href='/product/slug' passHref prefetch={false} legacyBehavior>
+          <Link>
             <CardActionArea>
-              <CardMedia
-                component='img'
-                image={`products/${product.images[0]}`}
-                alt={product.title}
-              />
+            <CardMedia
+              component="img"
+              className="fadeIn"
+              image={productImage}
+              alt={product.title}
+            />
             </CardActionArea>
-          </Grid>
-  )
-}
+          </Link>
+        </NextLink>
+      </Card>
+      <Box sx={{ mt: 1 }} className="fadeIn">
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>{`$${product.price}`}</Typography>
+      </Box>
+    </Grid>
+  );
+};
