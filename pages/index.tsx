6776f67f4f20858e0eefb24075ff621680892c1a
Andrés Murillo Cortázar <andresmurillocortazar@outlook.com>
2023-07-01 12:49:11 -0500

    Class 185

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,14 @@
 import { ShopLayout } from "@/components/layouts";
+import { ProductList } from "@/components/products";
 import { initialData } from "@/database/products";
-import { CardActionArea, CardMedia, Grid, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={"Encuentra los mejores productos de Teslo aquí"}>
       <Typography variant="h1" component='h1'>Tienda</Typography>
       <Typography variant="h2" sx={{marginBottom: 1}}>Todos los productos</Typography>
-      <Grid container spacing={4}>
-        {initialData.products.map(product=>(
-          <Grid item xs={6} sm={4} key={product.slug}>
-            <CardActionArea>
-              <CardMedia
-                component='img'
-                image={`products/${product.images[0]}`}
-                alt={product.title}
-              />
-            </CardActionArea>
-          </Grid>
-        ))}
-      </Grid>
+      <ProductList products={initialData.products as any}/>
     </ShopLayout>
   )
 }
