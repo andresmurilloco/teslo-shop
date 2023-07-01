e655113295a799b59fed6e1e58eca357aea794cc
Andrés Murillo Cortázar <andresmurillocortazar@outlook.com>
2023-07-01 17:07:44 -0500

    Class 194

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,4 +1,4 @@
-import { CartList } from "@/components/cart"
+import { CartList, OrdenSummary } from "@/components/cart"
 import { ShopLayout } from "@/components/layouts"
 import { Box, Button, Card, CardContent, Divider, Grid, Typography } from "@mui/material"
 
@@ -8,14 +8,14 @@ const CartPage = () => {
       <Typography variant="h1" component={'h1'}>Carrito</Typography>
       <Grid container>
         <Grid item xs={12} sm={7}>
-          <CartList/>
+          <CartList editable/>
         </Grid>
         <Grid item xs={12} sm={5}>
           <Card className="summary-card">
             <CardContent>
               <Typography variant="h2">Orden</Typography>
               <Divider sx={{my:1}}/>
-              {/* Order summary */}
+              <OrdenSummary/>
               <Box sx={{mt:3}}>
                 <Button color="secondary" className='circular-btn' fullWidth>Checkout</Button>
               </Box>
