5b2c438303fab4b9b4449874e459f2b2766a6065
Andrés Murillo Cortázar <andresmurillocortazar@outlook.com>
2023-07-01 12:10:52 -0500

    Curso 183

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,19 +1,19 @@
 import NextLink from 'next/link';
-import { AppBar, Box, Button, IconButton, Link, Toolbar, Typography } from "@mui/material"
-import { SearchOutlined } from '@mui/icons-material';
+import { AppBar, Badge, Box, Button, IconButton, Link, Toolbar, Typography } from "@mui/material"
+import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
 
 export const Navbar = () => {
   return (
     <AppBar>
         <Toolbar>
-            <NextLink href='/' passHref>
+            <NextLink href='/' passHref legacyBehavior>
                 <Link display='flex' alignItems='center'>
                     <Typography variant='h6'>Teslo |</Typography>
                     <Typography sx={{ml:0.5}}>Shop</Typography>
                 </Link>
             </NextLink>
             <Box flex={1}/>
-            <Box sx={{}}>
+            <Box sx={{display:{xs:'none', sm:'block'}}}>
                 <NextLink href='/category/men' passHref legacyBehavior>
                     <Link>
                         <Button>Hombres</Button>
@@ -34,6 +34,16 @@ export const Navbar = () => {
             <IconButton>
                 <SearchOutlined/>
             </IconButton>
+            <NextLink href='/cart' passHref legacyBehavior>
+                <Link>
+                    <IconButton>
+                        <Badge badgeContent={2} color='secondary'>
+                            <ShoppingCartOutlined/>
+                        </Badge>
+                    </IconButton>
+                </Link>
+            </NextLink>
+            <Button>Menú</Button>
         </Toolbar>
     </AppBar>
   )
---
+++ pages/404.tsx
@@ -0,0 +1,15 @@
+import { ShopLayout } from "@/components/layouts"
+import { Box, Typography } from "@mui/material"
+
+const Custom404 = () => {
+  return (
+    <ShopLayout title="Page not found" pageDescription="No hay nada aquí">
+        <Box display='flex' justifyContent='center' alignItems='center' height='calc(100vh-200px)' sx={{flexDirection:{xs:'column', sm:'row'}}}>
+            <Typography variant="h1" component='h1' fontSize={80} fontWeight={200}>404 | </Typography>
+            <Typography marginLeft={2}>No se ha encontrado esta pagina</Typography>
+        </Box>
+    </ShopLayout>
+  )
+}
+
+export default Custom404
