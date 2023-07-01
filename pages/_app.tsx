1da55d870c7a8adedd6cf8e8ec36775caf1ccde8
Andrés Murillo Cortázar <andresmurillocortazar@outlook.com>
2023-07-01 10:40:56 -0500

    First commit

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import { lightTheme } from '@/themes'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { ShopLayout } from "@/components/layouts";
+import { Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={"Encuentra los mejores productos de Teslo aquí"}>
+      <Typography variant="h1" component='h1'>Tienda</Typography>
+      <Typography variant="h2" sx={{marginBottom: 1}}>Todos los productos</Typography>
+    </ShopLayout>
+  )
+}
