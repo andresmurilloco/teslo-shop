574fc62cfa9a927e15ca31ff89550cb7c27d05ed
Andrés Murillo <andresmurillocortazar@outlook.com>
2023-07-01 19:30:29 -0500

    Proyecto terminado

---
+++ pages/auth/register.tsx
@@ -0,0 +1,50 @@
+import NextLink from "next/link";
+import { AuthLayout } from "@/components/layouts";
+import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
+
+const RegisterPage = () => {
+  return (
+    <AuthLayout title="Ingresar">
+      <Box sx={{ width: 350, padding: "10px 20px" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h1" component={"h1"}>
+              Registrate!
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="Nombre" variant="filled" fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="Correo" variant="filled" fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              variant="filled"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              color="secondary"
+              className="circular-btn"
+              size="large"
+              fullWidth
+            >
+              Registrarme
+            </Button>
+          </Grid>
+          <Grid item xs={12} display={"flex"} justifyContent={"end"}>
+            <NextLink href={"/auth/login"} passHref legacyBehavior>
+              <Link underline="always">Ya tengo una cuenta</Link>
+            </NextLink>
+          </Grid>
+        </Grid>
+      </Box>
+    </AuthLayout>
+  );
+};
+
+export default RegisterPage;
---
+++ pages/orders/history.tsx
@@ -0,0 +1,70 @@
+import NextLink from 'next/link'
+import { ShopLayout } from "@/components/layouts";
+import { Box, Chip, Grid, Link, Typography } from "@mui/material";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import React from "react";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 100 },
+  {
+    field: "paid",
+    headerName: "Pagado",
+    description: "Muestra si la orden ya fue pagada o no",
+    width: 200,
+    renderCell: (params:GridValueGetterParams)=>{
+        return (
+            params.row.paid ? <Chip color='success' label='Pagada' variant="outlined"/> : <Chip color='error' label='No pagada' variant="outlined"/>)
+    }
+  },
+  { field: "fullname", headerName: "Nombre Completo", width: 300 },
+  {
+    field: "orden",
+    headerName: "Ver orden",
+    sortable:false,
+    width: 200,
+    renderCell: (params:GridValueGetterParams)=>{
+        return (
+            <NextLink href={`/orders/${params.row.id}`} passHref legacyBehavior>
+                <Link underline='always'>Ver orden</Link>
+            </NextLink>
+        )
+    }
+  },
+];
+
+const rows = [
+  { id: 1, paid: false, fullname: "Andrés Murillo"},
+  { id: 2, paid: true, fullname: "Alfonso Lopez" },
+  { id: 3, paid: false, fullname: "Monica Galindo" },
+  { id: 4, paid: true, fullname: "Alberto Ramirez" },
+  { id: 5, paid: false, fullname: "Armando Paredes" },
+];
+
+const HistoryPage = () => {
+  return (
+    <ShopLayout
+      title="Historial de ordenes"
+      pageDescription="Historial de ordenes del cliente"
+    >
+      <Typography variant="h1" component={"h1"}>
+        Historial de ordenes
+      </Typography>
+      <Grid container>
+        <Grid item xs={12} sx={{ height: "650", width: "100%" }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: { pageSize: 5 },
+              },
+            }}
+            pageSizeOptions={[5, 10, 25]}
+          />
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default HistoryPage;
